8474d30f4a4a88a122c022212553c1bc305f95b9
Matta <104003130+Matta7@users.noreply.github.com>
2023-04-23 17:33:56 +0200

    Expedid main and enter function in ExpedidFacade

--- src/client/Expedid.java
+++ src/client/Expedid.java
@@ -1,7 +1,26 @@
 package client;
 
+import facade.ExpedidFacadeImpl;
+import facade.IExpedidFacade;
+
+import java.util.Scanner;
+
 public class Expedid {
     public static void main(String[] args) {
+        IExpedidFacade expedid = ExpedidFacadeImpl.getInstance();
+        Scanner scanner = new Scanner(System.in);
+        String command;
 
+        while (true) {
+            System.out.print("> ");
+            command = scanner.nextLine();
+            System.out.println(command);
+            String result = expedid.enter(command);
+            if (result == "quit") {
+                break;
+            } else {
+                System.out.println(result);
+            }
+        }
     }
 }
--- src/facade/ExpedidFacadeImpl.java
+++ src/facade/ExpedidFacadeImpl.java
@@ -1,5 +1,7 @@
 package facade;
 
+import java.util.Objects;
+
 public class ExpedidFacadeImpl implements IExpedidFacade {
 
     private static ExpedidFacadeImpl expedidFacade = new ExpedidFacadeImpl();
@@ -14,18 +16,66 @@ public class ExpedidFacadeImpl implements IExpedidFacade {
 
     @Override
     public String enter(String command) {
-        if (command == "!quit") {
-            return quit();
-        } else if (command.startsWith("!type")) {
-            return null;
-        }
+        if (Objects.equals(command, "!quit")) {
+            return this.quit();
 
-        return null;
+        } else if (command.startsWith("!type")) {
+            String[] separetedCommand = command.split(" ");
+            if(separetedCommand.length == 1) {
+                return this.type();
+            } else if (separetedCommand.length == 2) {
+                try {
+                    this.type(separetedCommand[1]);
+                    return ("Switched to "+ separetedCommand[1] + " type.");
+                } catch (IllegalArgumentException e) {
+                    return e.getMessage();
+                }
+            } else {
+                return "Error: Wrong number of argument.";
+            }
+
+        } else if (command.startsWith("!save")) {
+            String[] separetedCommand = command.split(" ");
+            if (separetedCommand.length == 2) {
+                try {
+                    this.save(separetedCommand[1]);
+                    return "Successfully saved file.";
+                } catch (IllegalArgumentException e) {
+                    return e.getMessage();
+                }
+            } else {
+                return "Error: Wrong number of argument.";
+            }
+
+        } else if (command.startsWith("!load")) {
+            String[] separetedCommand = command.split(" ");
+            if (separetedCommand.length == 2) {
+                try {
+                    this.load(separetedCommand[1]);
+                    return "Successfully loaded file.";
+                } catch (IllegalArgumentException e) {
+                    return e.getMessage();
+                }
+            } else {
+                return "Error: Wrong number of argument.";
+            }
+
+        } else if (command.startsWith("!")){
+            return "Error: Unknown command \"" + command.split(" ")[0] + "\"";
+
+        } else {
+            // Write something at the top of the stack.
+            try {
+                return null;
+            } catch (IllegalArgumentException e) {
+                return e.getMessage();
+            }
+        }
     }
 
     @Override
     public String quit() {
-        return null;
+        return "quit";
     }
 
     @Override
