38f0a137a141832805b017f83abe0ef4756d138b
Matta <104003130+Matta7@users.noreply.github.com>
2023-05-01 19:34:23 +0200

    Calc main

--- src/client/Calc.java
+++ src/client/Calc.java
@@ -1,7 +1,28 @@
 package client;
 
+import expression.ArithmeticExpression;
+import expression.ExpressionEvaluator;
+import expression.FunctionExpression;
+import expression.IExpression;
+import handler.ExpressionStackHandler;
+import xml.XMLManager;
+
 public class Calc {
+
     public static void main(String[] args) {
 
+        if (args.length == 1) {
+            ExpressionEvaluator evaluator = ExpressionEvaluator.getInstance();
+            System.out.println(evaluator.evaluate(XMLManager.load(args[0]), null));
+        } else if (args.length == 2) {
+            ExpressionEvaluator evaluator = ExpressionEvaluator.getInstance();
+            System.out.println(evaluator.evaluate(XMLManager.load(args[0]), args[1]));
+        } else {
+            System.out.println("Wrong number of argument.\n");
+        }
+        /*IExpression expression = new FunctionExpression();
+        expression.setExpression("9 x *");
+        ExpressionEvaluator evaluator = ExpressionEvaluator.getInstance();
+        System.out.println(evaluator.evaluate(expression, ""));*/
     }
 }
--- src/expression/ExpressionEvaluator.java
+++ src/expression/ExpressionEvaluator.java
@@ -1,79 +1,87 @@
 package expression;
 
+import expression.type.Type;
+
 import java.util.Stack;
 
 public class ExpressionEvaluator {
-  private static ExpressionEvaluator instance = new ExpressionEvaluator();
+    private static ExpressionEvaluator instance = new ExpressionEvaluator();
 
-  private ExpressionEvaluator() {}
+    private ExpressionEvaluator() {}
 
-  public static ExpressionEvaluator getInstance() {
-    return instance;
-  }
+    public static ExpressionEvaluator getInstance() {
+        return instance;
+    }
 
-  public String evaluate(IExpression expression, String xValue) {
-    String result = "";
-    if (expression.getToken().equals("arith")) {
-      result = calculateExpression(expression);
-    } else if (expression.getToken().equals("functional")) {
-      // Si "x" est dans l'expression, alors on vérifie que xValue ne soit pas null et on remplace toutes les occurences de "x" par xValue.
-      if (expression.getExpression().contains("x")) {
-        if (xValue != null) {
-          IExpression calculableExpression = new ArithmeticExpression();
-          calculableExpression.setExpression(expression.getExpression().replaceAll("x", xValue));
-          result = calculateExpression(calculableExpression);
-        } else {
-          result = "Invalid command, could not replace x, xValue is null";
+    public String evaluate(IExpression expression, String xValue) {
+        String result = "";
+        if (expression.getToken().equals(Type.ARITHMETIC)) {
+            result = calculateExpression(expression);
+        } else if (expression.getToken().equals(Type.FUNCTION)) {
+            // Si "x" est dans l'expression, alors on vérifie que xValue ne soit pas null et on remplace toutes les occurences de "x" par xValue.
+            if (expression.getExpression().contains("x")) {
+                if (xValue != null) {
+                    IExpression calculableExpression = new ArithmeticExpression();
+                    calculableExpression.setExpression(expression.getExpression().replaceAll("x", xValue));
+                    result = calculateExpression(calculableExpression);
+                } else {
+                    result = "Invalid command, could not replace x, xValue is null";
+                }
+            } else {
+                result = calculateExpression(expression);
+            }
+        } else if (expression.getToken().equals("rational")) {
+            result = "Invalid expression, cannot evaluate rational expressions";
         }
-      } else {
-        result = calculateExpression(expression);
-      }
-    } else if (expression.getToken().equals("rational")) {
-      result = "Invalid expression, cannot evaluate rational expressions";
+        return result;
     }
-    return result;
-  }
 
-  private String calculateExpression(IExpression expression) {
-    // On calcule le résultat de l'expression, il s'agit une chaine de caractères contenant des nombres sous forme de chaine de caractères,
-    // qui peuvent être convertis en nombres réels, grace a Double.parseDouble (voir ci-dessous) et de symboles d'opérateurs.
+    private String calculateExpression(IExpression expression) {
+        // On calcule le résultat de l'expression, il s'agit une chaine de caractères contenant des nombres sous forme de chaine de caractères,
+        // qui peuvent être convertis en nombres réels, grace a Double.parseDouble (voir ci-dessous) et de symboles d'opérateurs.
 
-    // La notation se fait en notation polonaise inverse, c'est à dire que les opérateurs sont placés après les opérandes.
+        // La notation se fait en notation polonaise inverse, c'est à dire que les opérateurs sont placés après les opérandes.
 
-    // On utilise une pile pour stocker les opérandes et les opérateurs.
-    Stack<String> stack = new Stack<String>();
-    // On sépare l'expression en un tableau de String, en utilisant l'espace comme séparateur.
-    String[] expressionArray = expression.getExpression().split(" ");
-    // On parcourt le tableau de String.
-    for (int i = 0; i < expressionArray.length; i++) {
-      // Si l'élément est un opérateur, on le dépile, on dépile les deux opérandes précédentes, on les convertit en nombres réels, on applique l'opérateur et on empile le résultat.
-      if (expressionArray[i].equals("+")) {
-        Double operand2 = Double.parseDouble(stack.pop());
-        Double operand1 = Double.parseDouble(stack.pop());
-        stack.push(Double.toString(operand1 + operand2));
-      } else if (expressionArray[i].equals("-")) {
-        Double operand2 = Double.parseDouble(stack.pop());
-        Double operand1 = Double.parseDouble(stack.pop());
-        stack.push(Double.toString(operand1 - operand2));
-      } else if (expressionArray[i].equals("*")) {
-        Double operand2 = Double.parseDouble(stack.pop());
-        Double operand1 = Double.parseDouble(stack.pop());
-        stack.push(Double.toString(operand1 * operand2));
-      } else if (expressionArray[i].equals("/")) {
-        Double operand2 = Double.parseDouble(stack.pop());
-        Double operand1 = Double.parseDouble(stack.pop());
-        stack.push(Double.toString(operand1 / operand2));
-      } else if (expressionArray[i].equals("~")) {
-        Double operand = Double.parseDouble(stack.pop());
-        stack.push(Double.toString(-operand));
-      } else {
-        // Si l'élément n'est pas un opérateur, on l'empile.
-        stack.push(expressionArray[i]);
-      }
+        // On utilise une pile pour stocker les opérandes et les opérateurs.
+        Stack<String> stack = new Stack<>();
+        // On sépare l'expression en un tableau de String, en utilisant l'espace comme séparateur.
+        String[] expressionArray = expression.getExpression().split(" ");
+        // On parcourt le tableau de String.
+        for (int i = 0; i < expressionArray.length; i++) {
+            // Si l'élément est un opérateur, on le dépile, on dépile les deux opérandes précédentes, on les convertit en nombres réels, on applique l'opérateur et on empile le résultat.
+            switch (expressionArray[i]) {
+                case "+" -> {
+                    Double operand2 = Double.parseDouble(stack.pop());
+                    Double operand1 = Double.parseDouble(stack.pop());
+                    stack.push(Double.toString(operand1 + operand2));
+                }
+                case "-" -> {
+                    Double operand2 = Double.parseDouble(stack.pop());
+                    Double operand1 = Double.parseDouble(stack.pop());
+                    stack.push(Double.toString(operand1 - operand2));
+                }
+                case "*" -> {
+                    Double operand2 = Double.parseDouble(stack.pop());
+                    Double operand1 = Double.parseDouble(stack.pop());
+                    stack.push(Double.toString(operand1 * operand2));
+                }
+                case "/" -> {
+                    Double operand2 = Double.parseDouble(stack.pop());
+                    Double operand1 = Double.parseDouble(stack.pop());
+                    stack.push(Double.toString(operand1 / operand2));
+                }
+                case "~" -> {
+                    Double operand = Double.parseDouble(stack.pop());
+                    stack.push(Double.toString(-operand));
+                }
+                default ->
+                    stack.push(expressionArray[i]);
+                // Si l'élément n'est pas un opérateur, on l'empile.
+            }
+        }
+        // On retourne le dernier élément de la pile, qui est le résultat de l'expression.
+        return stack.pop();
     }
-    // On retourne le dernier élément de la pile, qui est le résultat de l'expression.
-    return stack.pop();
-  }
 
   public Boolean isNullable(IExpression expression) throws IllegalArgumentException {
     if (!expression.getToken().equals("rational")) {
@@ -112,4 +120,5 @@ public class ExpressionEvaluator {
     // On retourne le dernier élément de la pile, qui est le résultat de l'expression.
     return stack.peek();
   }
+
 }
