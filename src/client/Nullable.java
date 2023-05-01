c1336b037acb5a55ad27c77d7850bd4f77e3d5c7
Matta <104003130+Matta7@users.noreply.github.com>
2023-05-01 19:49:41 +0200

    Nullable main

--- src/client/Nullable.java
+++ src/client/Nullable.java
@@ -1,7 +1,23 @@
 package client;
 
+import expression.ExpressionEvaluator;
+import expression.IExpression;
+import expression.RationalExpression;
+import xml.XMLManager;
+
 public class Nullable {
     public static void main(String[] args) {
 
+        /*if (args.length == 1) {
+            ExpressionEvaluator evaluator = ExpressionEvaluator.getInstance();
+            System.out.println(evaluator.evaluate(XMLManager.load(args[0]), null));
+        } else {
+            System.out.println("Wrong number of argument.\n");
+        }*/
+
+        IExpression expression = new RationalExpression();
+        expression.setExpression("a b .");
+        ExpressionEvaluator evaluator = ExpressionEvaluator.getInstance();
+        System.out.println(evaluator.isNullable(expression));
     }
 }
